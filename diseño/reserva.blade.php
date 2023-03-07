fd98393b36f34259545047385b7bf492de63c75c
Manuelo2 <manuel.llanorebanal@iesmiguelherrero.com>
2023-03-07 20:15:06 +0100

    cambios en reserva y js

--- diseño/reserva.blade.php
+++ diseño/reserva.blade.php
@@ -9,27 +9,58 @@
     <script type="text/javascript" src="reserva.js"></script>
     <link rel="stylesheet" href="reserva.css">
 </head>
+<style>
+    #mañana {
+        display: none;
+    }
+
+    .botonesReservaLibre {
+        background-color: #28A453;
+        width: 120px;
+        margin: 6px 45px 6px 30px;
+        padding: 10px;
+        height: 50px;
+        border-radius: 10px;
+        box-shadow: 3px 3px 8px black;
+        border: 0;
+        color: white;
+    }
+
+    .botonesReservaOcupado {
+        background-color: #A22929;
+        width: 120px;
+        margin: 6px 45px 6px 30px;
+        padding: 10px;
+        height: 50px;
+        border-radius: 10px;
+        box-shadow: 3px 3px 8px black;
+        border: 0;
+        color: white;
+    }
+
+    #tarde {
+        display: none;
+    }
+</style>
 
 <body>
-    <form action="reserva.html">
+    <form action="reserva.html" method="POST">
         <div id="usuario">
             <span id="textoConexion">Bienvenido xxxxxxx</span> <button class="botonRojo">Cerrar sesion</button>
         </div>
-        <fieldset>
+        <fieldset id="imagenesOrdenador">
             <legend><a class="centrar">Selecciona ordenador</a></legend>
-            <div id="imagenesOrdenador"></div>
         </fieldset>
         <fieldset>
             <legend><a class="centrar2">Escoge una fecha</a></legend>
             <input type="date" id="fechaReserva">
         </fieldset>
-        <fieldset>
+        <fieldset id="mañana">
             <legend><a class="centrar2">Turnos de mañana</a></legend>
-            <div id="turnosMañana" class="tamaño"></div>
         </fieldset>
-        <fieldset>
+        <fieldset id="tarde">
             <legend><a class="centrar2">Turnos de tarde</a></legend>
-            <div id="turnosTarde" class="tamaño"></div>
+
         </fieldset>
         <button id="borrar">Borrar seleccion</button>
         <button id="reser">Reservar</button>
--- diseño/reserva.js
+++ diseño/reserva.js
@@ -1,13 +1,36 @@
 addEventListener('load', cargar, false);
-    var fechaActual;
+var fechaActual;
+var fechaReserva;
+var turnosMañana = new Array("8:30", "9:20", "10:30", "11:20", "12:35", "13:25");
+var turnosTarde = new Array('14:45', '15:35', '16:40', '17:30', '18:35', '19:25')
 function cargar() {
+    fechaReserva = document.querySelector("#fechaReserva");
     fechaReserva.addEventListener("change", elegirFecha, false);
 }
 
 function elegirFecha() {
-    fecha = fechareserva.value;
+    fecha = new Date(Date.parse(fechaReserva.value));
     fechaActual = new Date(Date.now());
-    if (fecha.getYear) {
-        
+    if (fechaActual > fecha) {
+        alert(`La fecha ${fecha.toLocaleDateString('es')} es anterior a la actual`)
+        fechaReserva.value = null;
+    } else {
+        mañana.style.display = "block";
+        tarde.style.display = "block";
+        for (let i = 0; i < turnosMañana.length; i++) {
+            let inp = document.createElement("input");
+            inp.type = "button";
+            inp.value = turnosMañana[i];
+            inp.id = i;
+            inp.className = "botonesReservaLibre";
+            let inp2 = document.createElement("input");
+            inp2.type = "button";
+            inp2.value = turnosTarde[i];
+            inp2.id = i;
+            inp2.className = "botonesReservaLibre";
+            inp2.disabled = false;
+            mañana.appendChild(inp);
+            tarde.appendChild(inp2);
+        }
     }
 }
