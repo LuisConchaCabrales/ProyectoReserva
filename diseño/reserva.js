f54de42126ee72a10674fbc94323856de2043abf
Manuelo2 <manuel.llanorebanal@iesmiguelherrero.com>
2023-03-07 19:09:27 +0100

    Cambios prueba servidor

--- diseño/reserva.js
+++ diseño/reserva.js
@@ -1,10 +1,11 @@
 addEventListener('load', cargar, false);
-
+    var fechaActual;
 function cargar() {
     fechaReserva.addEventListener("change", elegirFecha, false);
 }
 
 function elegirFecha() {
     fecha = fechareserva.value;
+    fechaActual = new Date(Date.now());
     alert(fecha)
 }
