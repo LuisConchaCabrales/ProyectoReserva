0d5597d1c4b4818551fe8b95c21ebbcb10555da7
Manuelo2 <manuel.llanorebanal@iesmiguelherrero.com>
2023-03-08 16:38:11 +0100

    Ordenadores cargados

--- diseño/reserva.js
+++ diseño/reserva.js
@@ -2,8 +2,10 @@ addEventListener('load', cargar, false);
 var fechaActual;
 var fechaReserva;
 var turnosMañana = new Array("8:30", "9:20", "10:30", "11:20", "12:35", "13:25");
-var turnosTarde = new Array('14:45', '15:35', '16:40', '17:30', '18:35', '19:25')
+var turnosTarde = new Array('14:45', '15:35', '16:40', '17:30', '18:35', '19:25');
+var idPcElegido = 0;
 function cargar() {
+    introducirOrdenadores();
     fechaReserva = document.querySelector("#fechaReserva");
     fechaReserva.addEventListener("change", elegirFecha, false);
 }
@@ -14,6 +16,8 @@ function elegirFecha() {
     if (fechaActual > fecha) {
         alert(`La fecha ${fecha.toLocaleDateString('es')} es anterior a la actual`)
         fechaReserva.value = null;
+    } else if (idPcElegido = 0) {
+        alert("Selecciona el ordenador primero para ver sus horarios disponiles");
     } else {
         mañana.style.display = "block";
         tarde.style.display = "block";
@@ -33,4 +37,20 @@ function elegirFecha() {
             tarde.appendChild(inp2);
         }
     }
+}
+
+function introducirOrdenadores() {
+    for (let i = 0; i < 10; i++) {
+        let ima = document.createElement("img");
+        ima.src = "ordenador.png";
+        ima.className = "PC";
+        ima.id = i + 1;
+        ima.addEventListener("click", obtenerId, false);
+        imagenesOrdenador.appendChild(ima);
+    }
+}
+
+function obtenerId(e) {
+    idPcElegido = e.target.id;
+    alert(`Ordenador nº elegido ${idPcElegido}`);
 }
